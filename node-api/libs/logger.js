f29fb4b7f102425b27a633aa8d9292f6e32c2afc
moocss <moocss@gmail.com>
2017-02-20 09:04:56 +0800

    日志

--- node-api/libs/logger.js
+++ node-api/libs/logger.js
@@ -13,6 +13,10 @@ const exLogFile = path.join(__dirname, '../.logs/exceptions.json')
 
 // LEVELS : { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
 
+// const dateFormat = function () {
+//   return moment().format('YYYY-MM-DD HH:mm:ss:SSS');
+// }
+
 module.exports = new winston.Logger({
   transports: [
     new winston.transports.File({
@@ -27,6 +31,7 @@ module.exports = new winston.Logger({
     new winston.transports.Console({
       level: 'debug',
       timestamp: true,
+      // timestamp: dateFormat,
       handleExceptions: true,
       colorize: true,
     }),
